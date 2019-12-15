337628214af8053585f690a2da1720620c5028f9
MarkDijkstra <markdijkstra.eu@gmail.com>
2019-12-15 21:54:46 +0100

    added more grid and some minor colors tweaks and some better naming

--- src/app/components/pages/Home.js
+++ src/app/components/pages/Home.js
@@ -3,25 +3,64 @@ import React from "react";
 export class Home extends React.Component {
     render() {
         return <div className="x3_container--xl">
+            
                 <div className="x3_row">
-                    <div className="x3_col--sm--6">
-                        <div className="card">left</div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">1</div>
                     </div>
-                    <div className="x3_col--sm--6">
-                        <div className="card">
-                             <a href="#" className="x3_btn x3_btn--primary x3_btn--xl">button</a>
-            <br/>
-            <a href="#" className="x3_btn x3_btn--secondary x3_btn--xl">secondary</a>
-            <br/>
-            <a href="#" className="x3_btn x3_btn--green x3_btn--xl">green</a>
-            <br/>
-            <a href="#" className="x3_btn x3_btn--red x3_btn--xl">red</a>
-                         </div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">2</div>
+                    </div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">3</div>
+                    </div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">4</div>
+                    </div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">5</div>
+                    </div>
+                    <div className="x3_col--sm--2">
+                        <div className="card">6</div>
                     </div>
                 </div>
+            
+                <div className="x3_row">
+                    <div className="x3_col--sm--3">
+                        <div className="card">1</div>
+                    </div>
+                    <div className="x3_col--sm--3">
+                        <div className="card">2</div>
+                    </div>
+                    <div className="x3_col--sm--3">
+                        <div className="card">3</div>
+                    </div>
+                    <div className="x3_col--sm--3">
+                        <div className="card">4</div>
+                    </div>
+                </div>
+        
+                <div className="x3_row">
+                    <div className="x3_col--sm--6">
+                        <div className="card">1</div>
+                    </div>
+                    <div className="x3_col--sm--6">
+                        <div className="card">2</div>
+                    </div>
+                </div>
+        
                 <div className="x3_row">
                     <div className="x3_col--sm--12">
-                        <div className="card">full</div>
+                        <div className="card">                            
+                            
+                            <a href="#" className="x3_btn x3_btn--primary x3_btn--xl x3_mr--2">Primary</a>
+                            <a href="#" className="x3_btn x3_btn--secondary x3_btn--xl x3_mr--2">Secondary</a>
+                            <a href="#" className="x3_btn x3_btn--succes x3_btn--xl x3_mr--2">Succes</a>
+                            <a href="#" className="x3_btn x3_btn--danger x3_btn--xl x3_mr--2">Danger</a>
+                            <a href="#" className="x3_btn x3_btn--alert x3_btn--xl x3_mr--2">Alert</a>
+                            <a href="#" className="x3_btn x3_btn--info x3_btn--xl x3_mr--2">Info</a>
+        
+                        </div>
                     </div>
                 </div>
             </div>;
--- src/app/index.js
+++ src/app/index.js
@@ -1,39 +1,13 @@
 import React from "react";
 import {render} from "react-dom";
-import { BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams} from "react-router-dom";
-// import {Router, Route,  IndexRoute} from "react-router";
-//import { createBrowserHistory } from "history";
-
-//const history = createBrowserHistory();
+import {BrowserRouter as Router,
+        Switch,
+        Route,
+        Link,
+        useRouteMatch,
+        useParams} from "react-router-dom";
 
 import {App} from "./App";
 
-// class Root extends React.Component {
-//     render() {
-//         return <Router history={history}>
-//         <Route path={"/"} component={App} >
-//             <Route path={"users/:id"} component={Users} />
-//             <Route path={"home"} component={Home} />
-//             <Route path={"pages"} component={Pages} />
-//         </Route>
-//         <Route path={"home-single"} component={Home}/>
-//         </Router>;
-//     }
-//
-// }
-
-// class Root extends React.Component {
-//     render() {
-//         return
-//           <Router>
-//         <App/>
-//         </Router>;
-//     }
-// }
 render(<App />, window.document.getElementById('root'));
 console.log('index running');
